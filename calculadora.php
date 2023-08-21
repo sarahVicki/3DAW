25bb3657a1dd36a21941856c1bfacb0559e6b109
sarahVicki <142837608+sarahVicki@users.noreply.github.com>
2023-08-21 16:23:51 -0300

    Update calculadoraSoma.php

--- calculadora.php
+++ calculadora.php
@@ -2,13 +2,13 @@
 <html>
 	<head>
 		<meta charset="UTF-8">
-		<title>Calculadora</title>
+		<title>Calculadora-soma</title>
 	</head>
 	<body>
-		<h1>Calculadora</h1>
+		<h1>Calculadora - soma</h1>
 		<form action="calculadora.php" method="GET">
 		operador 1: <input type="text" name="op1"> + <br/>
-		operador 2: <input type="text" name="op2"> + <br/>
+		operador 2: <input type="text" name="op2"> <br/>
 		<input type="submit" value="soma">
 
 	<?php
@@ -16,17 +16,6 @@
 		$v2 = $_GET["op2"];
 		$result = $v1 + $v2;
 		echo "<h1> $result </h1>";
-
-		switch (operacao) {
-		case 1: $result = $v1 + $v2;
-		break; 
-		case 2: $result = $v1 - $v2;
-		break; 
-		case 3: $result = $v1 * $v2;
-		break; 
-		case 4: $result = $v1 / $v2;
-		break; 
-		}
 	?>
 	</body>
 </html>
